c0f9e98d0d65341e48e145c45437e621580c2123
xuhaojie <xuhaojie@hotmail.com>
2022-10-25 04:37:59 +0800

    add install-sudo script

---
+++ alpine/scripts/alpine/install-sudo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]] 
+then
+	echo "Error:This script must be run as root!" 1>&2
+	exit 1
+fi
+
+if [ $# -eq 0 ]; then
+	echo "please speicfy user name to be sudoer"
+	exit 1
+fi
+
+apk add sudo
+
+echo "add $1 to group wheel"
+addgroup $1 wheel
+
+
+
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
+
