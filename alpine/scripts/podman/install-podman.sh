f5b8a9e22f934903fea8dab743a6485dd26eedcd
xuhaojie <xuhaojie@hotmail.com>
2022-10-25 05:46:15 +0800

    add install podman script

---
+++ alpine/scripts/podman/install-podman.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]] 
+then
+	echo "Error:This script must be run as root!" 1>&2
+	exit 1
+fi
+
+if [ $# -eq 0 ]; then
+	echo "please speicfy user name to setup"
+	exit 1
+fi
+
+apk add podman
+rc-update add cgroups
+rc-service cgroups start
+
+modprobe tun
+echo tun >>/etc/modules
+
+echo $1:100000:65536 >/etc/subuid
+
+echo $1:100000:65536 >/etc/subgid
