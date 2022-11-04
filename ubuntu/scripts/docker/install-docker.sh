fd4360d12d6569226c9de2cadecacb2753308ea0
xuhaojie <xuhaojie@hotmail.com>
2022-11-04 17:54:37 +0800

    尝试添加nvidia runtime支持

--- ubuntu/scripts/docker/install-docker.sh
+++ ubuntu/scripts/docker/install-docker.sh
@@ -19,6 +19,19 @@ else
 	addgroup $1 docker
 fi
 
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+
+service docker stop
+
+#dockerd --add-runtime=nvidia=/usr/bin/nvidia-container-runtime
+
 service docker start
 
 # verify
