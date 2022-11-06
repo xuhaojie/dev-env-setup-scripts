f1c20dbe845db7c578ddc3b9e636f9ce03f89ab6
xuhaojie <xuhaojie@hotmail.com>
2022-11-06 22:02:12 +0800

    修正bug

--- ubuntu/scripts/ubuntu/install-minicom.sh
+++ ubuntu/scripts/ubuntu/install-minicom.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ $EUID -eq 0 ]] 
+then
+	echo "Error:This script cannot be run as root!" 1>&2
+	exit 1
+fi
+
 sudo apt install -y minicom 
 
-sudo usermod -aG dialout ${USER}
+CUR_USER=$USER
+echo "add user $CUR_USER to dialout group"
+sudo usermod -aG dialout $CUR_USER
--- ubuntu/scripts/ubuntu/serial-port.sh
+++ ubuntu/scripts/ubuntu/serial-port.sh
@@ -1,1 +1,11 @@
-sudo usermod -aG dialout ${USER}
+#!/bin/bash
+
+if [[ $EUID -eq 0 ]] 
+then
+	echo "Error:This script cannot be run as root!" 1>&2
+	exit 1
+fi
+
+CUR_USER=$USER
+echo "add user $CUR_USER to dialout group"
+sudo usermod -aG dialout $CUR_USER
